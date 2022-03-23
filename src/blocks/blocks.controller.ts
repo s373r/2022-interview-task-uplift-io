8782db4dd8136f8aaa99a49a5d954d2949a28e27
Dima Pristupa <dima.pristupa@pm.me>
2022-03-23 21:14:26 +0200

    🚧 BlocksController: use ParseBlockIdPipe

--- src/blocks/blocks.controller.ts
+++ src/blocks/blocks.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { BlockTransactionString } from 'web3-eth';
 
+import { ParseBlockIdPipe } from '../parse-block-id.pipe';
 import { BlockNumber, Web3Service } from '../web3/web3.service';
 
 @Controller('blocks')
 export class BlocksController {
   constructor(private readonly web3Service: Web3Service) {}
 
+  // TODO: docs: 400 if id is not ParseBlockIdPipe
   @Get(':id')
   async getBlock(
-    // TODO: it is ok for the type-checker but we need to add runtime checks as well
-    @Param('id') id: BlockNumber,
+    @Param('id', ParseBlockIdPipe) id: BlockNumber,
   ): Promise<BlockTransactionString> {
     return this.web3Service.getBlock(id);
   }
