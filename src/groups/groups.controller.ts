c5d4308bd66574802775f288e2d858f053156fd6
Dima Pristupa <dima.pristupa@pm.me>
2022-03-23 22:01:14 +0200

    🚧 GroupsController.getGroupIds(): implement

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -5,11 +5,16 @@ import {
   Param,
 } from '@nestjs/common';
 
+import { GroupIds } from '../web3/contracs/blockchainIndexesContract';
+import { Web3Service } from '../web3/web3.service';
+
 @Controller('groups')
 export class GroupsController {
+  constructor(private readonly web3Service: Web3Service) {}
+
   @Get()
-  getGroupIds(): void {
-    throw new NotImplementedException();
+  async getGroupIds(): Promise<GroupIds> {
+    return this.web3Service.getGroupIds();
   }
 
   @Get(':id')
--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { GroupsController } from './groups.controller';
+import { Web3Module } from '../web3/web3.module';
 
 @Module({
+  imports: [Web3Module],
   controllers: [GroupsController],
 })
 export class GroupsModule {}
