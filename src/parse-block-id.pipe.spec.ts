852f47fdd72707af5c12e6c5a9553ca15b524c81
Dima Pristupa <dima.pristupa@pm.me>
2022-03-23 21:13:53 +0200

    🚧 ParseBlockIdPipe: implement

---
+++ src/parse-block-id.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ParseBlockIdPipe } from './parse-block-id.pipe';
+
+describe('ParseBlockIdPipe', () => {
+  it('should be defined', () => {
+    // TODO: tests
+    expect(new ParseBlockIdPipe()).toBeDefined();
+  });
+});
---
+++ src/parse-block-id.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  BadRequestException,
+} from '@nestjs/common';
+import { BlockNumber } from './web3/web3.service';
+
+@Injectable()
+export class ParseBlockIdPipe implements PipeTransform<string> {
+  transform(value: string, metadata: ArgumentMetadata): BlockNumber {
+    if (value === 'latest') {
+      return value;
+    }
+
+    const convertedValue = +value;
+    const isInteger = Number.isInteger(convertedValue);
+
+    if (!isInteger) {
+      throw new BadRequestException(
+        'Validation failed (integer or "latest" string is expected)',
+      );
+    }
+
+    return convertedValue;
+  }
+}
