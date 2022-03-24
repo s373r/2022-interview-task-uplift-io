2230aa7a4b78da00320e65619ab0299f5e242ede
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 16:52:15 +0200

    📝 Swagger: groups/ add HTTP statuses

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -2,12 +2,13 @@ import {
   CacheInterceptor,
   Controller,
   Get,
+  HttpStatus,
   NotFoundException,
   Param,
   ParseIntPipe,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { isNil } from 'lodash';
 
 import { ApiGroup } from '../common';
@@ -21,11 +22,27 @@ export class GroupsController {
   constructor(private readonly web3Service: Web3Service) {}
 
   @Get()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Group IDs are returned',
+  })
   async getGroupIds(): Promise<GroupIds> {
     return this.web3Service.getGroupIds();
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The requested group is returned',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '"id" is not a number',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'The requested group is not found',
+  })
   async getGroup(@Param('id', ParseIntPipe) id: number): Promise<Group> {
     const group = await this.web3Service.getGroup(id);
 
