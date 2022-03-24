69fbecec80031c9c86e4c404254d6c00057b314a
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 16:53:17 +0200

    📝 Swagger: blocks/ add HTTP statuses

--- src/blocks/blocks.controller.ts
+++ src/blocks/blocks.controller.ts
@@ -2,10 +2,11 @@ import {
   CacheInterceptor,
   Controller,
   Get,
+  HttpStatus,
   Param,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BlockTransactionString } from 'web3-eth';
 
 import { ApiGroup } from '../common';
@@ -18,8 +19,26 @@ import { BlockNumber, Web3Service } from '../web3/web3.service';
 export class BlocksController {
   constructor(private readonly web3Service: Web3Service) {}
 
-  // TODO: docs: 400 if id is not ParseBlockIdPipe
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The requested block is returned',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '"id" is not a valid block ID',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'The requested group is not found',
+  })
+  @ApiParam({
+    name: 'id',
+    // TODO: The following schema does not work -- figure out why
+    schema: {
+      oneOf: [{ type: 'string', enum: ['latest'] }, { type: 'integer' }],
+    },
+  })
   async getBlock(
     @Param('id', ParseBlockIdPipe) id: BlockNumber,
   ): Promise<BlockTransactionString> {
