7eb7fa55b91906f4a21ca74964a87435ff9d624a
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 20:41:15 +0200

    💚 Import required modules for tests [2]

--- src/web3/web3.service.spec.ts
+++ src/web3/web3.service.spec.ts
@@ -1,11 +1,15 @@
+import { CacheModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { Web3Service } from './web3.service';
+import { ConfigModule } from '../config/config.module';
 
 describe('Web3Service', () => {
   let service: Web3Service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register({ isGlobal: true }), ConfigModule],
       providers: [Web3Service],
     }).compile();
 
