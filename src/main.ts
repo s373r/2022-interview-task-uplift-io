fb9e57053b221d1fbf4c674743fb3083c31bc081
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 16:06:02 +0200

    📝 Swagger: add tags

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,21 @@ import { AppModule } from './app.module';
 import { AppConfigService, ConfigVariable } from './config';
 
 import packageJson from '../package.json';
+import { ApiGroup } from './common';
 
 async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle(packageJson.name)
     .setDescription(packageJson.description)
     .setVersion(packageJson.version)
+    .addTag(ApiGroup.BLOCKS)
+    .addTag(ApiGroup.GROUPS)
+    .addTag(ApiGroup.INDEXES)
     .build();
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup(ApiGroup.DOCUMENTATION, app, document);
 
   const configService = app.get<AppConfigService>(ConfigService);
   const port = configService.get(ConfigVariable.PORT, { infer: true });
