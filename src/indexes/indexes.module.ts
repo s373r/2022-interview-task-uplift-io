45fcb55ac16b113bb9f6427f29f9980d759cdc05
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 12:49:34 +0200

    🚧 IndexesModule: import Web3Module

--- src/indexes/indexes.module.ts
+++ src/indexes/indexes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { IndexesController } from './indexes.controller';
+import { Web3Module } from '../web3/web3.module';
 
 @Module({
+  imports: [Web3Module],
   controllers: [IndexesController],
 })
 export class IndexesModule {}
