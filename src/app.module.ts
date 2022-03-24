da5eeb9220a708cf94df45375d6b5ec08f49e6c6
Dima Pristupa <dima.pristupa@pm.me>
2022-03-24 13:06:35 +0200

    🔥 AppController, AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlocksModule } from './blocks/blocks.module';
 import { ConfigModule } from './config/config.module';
 import { GroupsModule } from './groups/groups.module';
 import { IndexesModule } from './indexes/indexes.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [ConfigModule, BlocksModule, IndexesModule, GroupsModule],
 })
 export class AppModule {}
